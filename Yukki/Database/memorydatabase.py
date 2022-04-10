447a01ed9813be39c3d3fad1afed5cba7470ac50
Cute Inspire <Randywahidin321@gmail.com>
2022-04-10 14:17:43 +0700

    is_active_chat

--- Yukki/Database/memorydatabase.py
+++ Yukki/Database/memorydatabase.py
@@ -40,3 +40,24 @@ async def autoend_off():
     user = await autoenddb.find_one({"chat_id": chat_id})
     if user:
         return await autoenddb.delete_one({"chat_id": chat_id})
+
+# Active Voice Chats
+async def get_active_chats() -> list:
+    return active
+
+
+async def is_active_chat(chat_id: int) -> bool:
+    if chat_id not in active:
+        return False
+    else:
+        return True
+
+
+async def add_active_chat(chat_id: int):
+    if chat_id not in active:
+        active.append(chat_id)
+
+
+async def remove_active_chat(chat_id: int):
+    if chat_id in active:
+        active.remove(chat_id)
